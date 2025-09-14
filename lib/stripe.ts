c9e9a67c65a9ad9eb179448fabbb2db8a644bd92
mrumm <markus-rummel@gmx.de>
2025-09-14 13:24:28 -0400

    added receipt email to stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -39,6 +39,7 @@ export async function createCheckoutSession({
     success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/booking/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/booking`,
     customer_email: customerEmail,
+    receipt_email: customerEmail,
     metadata: {
       startDate,
       endDate,
